a720ce577565d904072fed63ae938852ff5c16ee
Benjamin Kabondo <benjaminkabondo@Benjamins-MacBook-Pro.local>
2026-03-02 01:40:44 -0600

    prepush

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>{{ message }}</h1>
+  <img v-bind:src="imageUrl" alt="" />
+  <br>
+  <img :src="imageUrl" alt="" />
+
+  <button @click=""changImg>Change image</button>
 </template>
 
 <script setup>
+import {ref} from 'vue'
+let message = 'Hello v-bind!'
 
+let imageUrl='/public/img/banner_1.jpg'
+
+function changImg(){
+imageUrl.value='/public/img/banner_2.jpg'
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+img {
+  max-width: 300px;
+}
 
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
 </style>
